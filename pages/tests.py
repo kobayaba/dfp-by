d5b2e8f7d280003c54bdb8f1307217e49ee02dca
kobayaba <yabakoba@gmail.com>
2020-12-22 00:47:35 +0000

    Mise en place des fichier static

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomePagesTest(SimpleTestCase):
 	
@@ -25,4 +25,30 @@ class HomePagesTest(SimpleTestCase):
 		self.assertEqual(
 			view.func.__name__,
 			HomePageView.as_view().__name__
+		)
+
+
+class AboutPageTests(SimpleTestCase):
+
+	def setUp(self):
+		url = reverse('about')
+		self.response = self.client.get(url)
+
+	def test_aboutpage_status_code(self):
+		self.assertEqual(self.response.status_code, 200)
+
+	def test_aboutpage_template(self):
+		self.assertTemplateUsed(self.response, 'about.html')
+
+	def test_aboutpage_contains_correct_html(self):
+		self.assertContains(self.response, 'About Page')
+
+	def test_aboutpage_does_not_contain_incorrerct_html(self):
+		self.assertNotContains(self.response, 'je suis pas dans la page')
+
+	def test_aboutpage_url_reolves_aboutpageview(self):
+		view = resolve('/about/')
+		self.assertEqual(
+			view.func.__name__,
+			AboutPageView.as_view().__name__
 		)
